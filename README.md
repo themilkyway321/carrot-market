5922827b8f29f4408f6a6107a62ecea93fb2edd6
themilkyway321 <alfl05@hanmail.net>
2023-06-29 00:01:00 +0900

    second

--- README.md
+++ README.md
@@ -49,3 +49,129 @@ button class="... ring-2 ring-blue-500"
 button class="... ring-2 ring-blue-500/50
 ```
 https://tailwindcss.com/docs/ring-color
+
+
+
+:empty
+
+empty modifier를 사용하여 콘텐츠가 없는 경우 스타일을 지정합니다.
+콘텐츠가 없는 경우=>빈 텍스트, undefined, null등과 같이 값이 없는 경우에 해당
++ empty:hidden은 display: none과 같다.
+```
+{[1, 2, ""].map((number) => (
+< h3 className="bg-red-500 empty:hidden" key={number}>
+{number}
+< /h3>
+))}
+```
+https://tailwindcss.com/docs/hover-focus-and-other-states#empty
+
+짝수=> even:bg-blue-200
+홀수=> odd:bg-red-200
+
+
+
+group
+상위(부모) 상태를 기반으로 한 스타일 지정
+일부 부모 요소의 상태를 기반으로 요소의 스타일을 지정해야 하는 경우 부모를 group 클래스로 표시하고 group-hover와 같은 group-* 수정자를 사용하여 대상 요소의 스타일을 지정합니다.
+이 패턴은 group-focus, group-active 또는 group-odd와 같은 모든 유사 클래스 수정자와 함께 작동합니다.
+```
+< a href="#" class="group">
+< h3 class="group-hover:text-white">New project< /h3>
+< /a>
+```
+https://tailwindcss.com/docs/hover-focus-and-other-states#styling-based-on-parent-state
+
+peer
+형제 상태를 기반으로 한 스타일 지정
+형제 요소의 상태를 기반으로 요소의 스타일을 지정해야 하는 경우 형제를 peer 클래스로 표시하고 peer-invalid와 같은 peer-* 수정자를 사용하여 대상 요소의 스타일을 지정합니다. 이 패턴은 모든 유사 클래스 수정자(예: peer-focus, peer-required 및 peer-disabled)와 함께 작동합니다.
+< input class="peer"/ >
+< p class="peer-invalid:visible"> Pizza< /p>
+```
+https://tailwindcss.com/docs/hover-focus-and-other-states#styling-based-on-sibling-state
+
+
+details
+HTML details 요소는 "열림" 상태일 때만 내부 정보를 보여주는 정보 공개 위젯을 생성합니다. 요약이나 레이블은 summary 요소를 통해 제공할 수 있습니다. 정보 공개 위젯은 보통 레이블 옆의 작은 삼각형이 돌아가면서 열림/닫힘 상태를 나타냅니다. details 요소의 첫 번째 자식이 summary 요소라면, summary의 콘텐츠를 위젯의 레이블로 사용합니다.
+```
+< details>
+< summary>Details
+Something small enough to escape casual notice.
+< /details>
+```
+https://developer.mozilla.org/ko/docs/Web/HTML/Element/details
+
+File input buttons
+파일 수정자를 사용하여 파일 입력의 버튼 스타일 지정
+ex) file:mr-4 file:py-2 file:px-4
+https://tailwindcss.com/docs/hover-focus-and-other-states?email=george%40krugerindustrial&password=Bosco#file-input-buttons
+
+::file-selector-button
+::file-selector-button CSS 의사 요소는 type="file"의 input 버튼을 나타냅니다.
+ex) input[type=file]::file-selector-button
+https://developer.mozilla.org/en-US/docs/Web/CSS/::file-selector-button
+
+
+obile First
+
+기본적으로 Tailwind는 Bootstrap과 같은 다른 프레임워크에서 사용하는 것과 유사한 모바일 우선 breakpoint 시스템을 사용합니다. 이것이 의미하는 바는 접두사가 붙지 않은 유틸리티(예: uppercase)는 모든 화면 크기에 적용되는 반면 접두사가 붙은 유틸리티(예: md:uppercase)는 지정된 breakpoint 이상에서만 적용됩니다.
+
+이 접근 방식이 사람들을 가장 자주 놀라게 하는 부분은 모바일용으로 스타일을 지정하려면 sm: 접두사가 붙은 버전이 아니라 접두사가 없는 버전의 유틸리티를 사용해야 한다는 것입니다. sm을 "작은 화면에서"를 의미하는 것으로 생각하지 마십시오. "작은 breakpoint"로 생각하십시오.
+div class="sm:text-center" => 작은 사이즈 (not 모바일)
+
+이러한 이유로 디자인을 위한 모바일 레이아웃을 먼저 구현한 다음 sm 화면에 적합한 변경 사항을 레이어링한 다음 md 화면 등을 적용하는 것이 좋습니다.
+```
+sm 640px @media (min-width: 640px) { ... }
+md 768px @media (min-width: 768px) { ... }
+lg 1024px @media (min-width: 1024px) { ... }
+xl 1280px @media (min-width: 1280px) { ... }
+2xl 1536px @media (min-width: 1536px) { ... }
+```
+https://tailwindcss.com/docs/responsive-design#mobile-first
+
+Customizing breakpoints
+https://tailwindcss.com/docs/responsive-design#customizing-breakpoints
+
+
+Dark Mode
+Tailwind에는 dark 모드가 활성화되어 있을 때 사이트 스타일을 다르게 지정할 수 있습니다. 현재 사용 중인 컴퓨터에서 설정한 라이트 모드 또는 다크 모드에 따라 dark가 자동으로 적용됩니다.
+ex) dark:bg-slate-900
+https://tailwindcss.com/docs/dark-mode
+
+수동으로 다크 모드 전환
+운영 체제 기본 설정에 의존하는 대신 수동으로 다크 모드 전환을 지원하려면 media 대신 class을 사용하십시오.
+```
+// tailwind.config.js
+module.exports = {
+// 클래스를 기준으로 다크모드 적용 (최상위 부모에 dark클래스 지정)
+darkMode: 'class',
+
+// @media(prefers-color-scheme)를 기준으로 다크모드 적용 (기본 값)
+darkMode: "media",
+}
+```
+https://tailwindcss.com/docs/dark-mode#toggling-dark-mode-manually
+
+prefers-color-scheme
+prefers-color-scheme CSS 미디어 특성은 사용자의 시스템이 라이트 테마나 다크 테마를 사용하는지 탐지하는 데에 사용됩니다.
+```
+@media (prefers-color-scheme: light) {
+.themed {
+background: white;
+color: black;
+}
+}
+```
+https://developer.mozilla.org/ko/docs/Web/CSS/@media/prefers-color-scheme
+
+
+Migrating to the JIT engine
+
+2021년 3월에 발표한 새로운 Just-in-Time 엔진이 Tailwind CSS v3.0의 클래식 엔진을 대체했습니다. 새로운 엔진은 프로젝트에 필요한 스타일을 주문형으로 생성합니다.
+
+Tailwind CSS v3.0 이전: 거대한 CSS파일을 생성하고, 그 파일에 이미 정의해놓은 클래스들을 가져와 사용하는 방식.
+대략 20만줄 정도 되는 클래스로 가득찬 파일을 가져와 개발 단계에서 사용하기 때문에 매우 무겁고, 배포 전에는 purge를 해줘야 해서 번거로움
+
+Tailwind CSS v3.0이후: 사용자가 사용하는 스타일들만 그때 그때 생성해서 사용하는 방식. 여러 클래스들을 조합해서 사용할 수 있고, 매우 가볍고, 배포 전 purge를 해주지 않아도 되서 편함
+
+https://tailwindcss.com/docs/upgrade-guide#migrating-to-the-jit-engine
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,25 +2,27 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return ( 
-  <div className="bg-slate-400 py-20 px-20 grid gap-10 min-h-screen">
-    <div className="bg-white p-6 rounded-3xl shadow-2xl">
-      <span className="font-semibold text-3xl">Select Item</span>
-      <div className="flex justify-between my-2">
-        <span className="text-gray-500">Gray Chair</span>
-        <span className="font-semibold">$19</span>
-      </div>
-      <div className="flex justify-between">
-        <span className="text-gray-500">Gray Table</span>
-        <span className="font-semibold">$10</span>
-      </div>
+  <div className="bg-slate-400 py-20 px-20 grid gap-10 lg:grid-cols-2 xl:grid-cols-3 min-h-screen xl:place-content-center dark">
+    <div className="bg-white dark:bg-black p-6 rounded-3xl shadow-2xl sm:bg-red-500 md:bg-teal-500 lg:bg-purple-500 xl:bg-pink-500">
+      <span className="font-semibold dark:text-white text-3xl">Select Item</span>
+      
+        <ul>
+          {[1,2,3,4,5].map((i)=>(
+            <div key={i} className="flex justify-between my-2 first:bg-blue-50 last:bg-blue-50 ">
+              <span className="text-gray-500 dark:text-gray-100">Gray Chair</span>
+                <span className="font-semibold dark:text-gray-100">$19</span>
+            </div>
+          ))}
+        </ul>
+          
       <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
         <span>Total</span>
         <span className="font-semibold">$10</span>
       </div>
-      <button className="block mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-1/2 mx-auto hover:bg-teal-500 hover:text-black active:bg-yellow-500 focus:text-red-500">Checkout</button>
+      <button className="block mt-5 bg-blue-500 dark:bg-black dark:border-white  text-white p-3 text-center rounded-xl w-1/2 mx-auto hover:bg-teal-500 hover:text-black active:bg-yellow-500 focus:text-red-500">Checkout</button>
     </div>
-    <div className="bg-white overflow-hidden rounded-2xl shadow-2xl">
-      <div className="bg-blue-500 p-6 pb-14">
+    <div className="bg-white overflow-hidden rounded-2xl shadow-2xl group ">
+      <div className="bg-blue-500 p-6 pb-14 xl:pb-80 portrait:bg-yellow-500 landscape:bg-teal-500">
         <span className="text-white text-2xl">Profile</span>
       </div>
       <div className="rounded-3xl relative -top-5 bg-white p-6">
@@ -29,7 +31,7 @@ const Home: NextPage = () => {
             <span className="text-sm text-gray-500">Orders</span>
             <span className="font-medium">340</span>
           </div>
-          <div className="h-24 w-24 bg-red-400 rounded-full"/>
+          <div className="h-24 w-24 bg-red-400 rounded-full group-hover:bg-red-100 transition-colors"/>
           <div className="flex flex-col items-center">
             <span className="text-sm text-gray-500">Spent</span>
             <span className="font-medium">$2,310</span>
@@ -41,7 +43,7 @@ const Home: NextPage = () => {
         </div>
       </div>
     </div>
-    <div className="bg-white p-10 rounded-2xl shadow-2xl">
+    <div className="bg-white p-10 rounded-2xl shadow-2xl lg:col-span-2 xl:col-span-1">
       <div className="flex justify-between items-center mb-4">
         <span className="shadow-xl p-2 rounded-md">⬅️</span>
         <div className="space-x-5">
@@ -72,6 +74,20 @@ const Home: NextPage = () => {
       </div>
     </div>
   </div>
+  // form
+  // <form className="flex flex-col space-y-2 p-5 h-screen">
+  //   <input type="text" required placeholder="Username" className="border-gray-400 rounded peer" />
+  //   <span className="hidden peer-invalid:block peer-invalid:text-red-500">This input is invalid.</span>
+  //   <span className="hidden peer-valid:block peer-valid:text-teal-500">Awesome username</span>
+  //   <input type="submit" value="Login" className="bg-white" />
+  // </form>
+
+  // <div className="flex flex-col space-y-2 p-5 h-screen">
+  //   <details className="select-none open:bg-indigo-400">
+  //     <summary className="cursor-pointer ">What is my fav. food?</summary>
+  //   <input type="file" className="file:cursor-pointer file:hover:text-purple-400 file:hover:bg-white file:transition-colors file:border-0 file:rounded-xl file:px-5 file:text-white file:bg-purple-400 bg-red-4009" />
+  //   </details>
+  // </div>
   );
 };
 export default Home;
