9735839d807a7d491d20d56876902df3d65a516e
DESKTOP-LKCJNBK\alfl05 <alfl05@hanmail.net>
2023-07-11 20:23:57 +0900

    doing prisma

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -6,8 +6,67 @@ async function handler(
   req:NextApiRequest, 
   res:NextApiResponse
 ){
-console.log(req.body);
-res.status(200).end();
+const {phone, email}=req.body;
+const payload = phone? {phone:+phone} :{email}
+const user = await client.user.upsert({
+  where:{
+    ...payload
+  },
+  create:{
+    name:"anyone",
+    ...payload,
+  },
+  update:{},
+});
+const token = await client.token.create({
+  data:{
+    payload:"12345",
+    user:{
+      connect:{
+        id:user.id,
+      }
+    },
+  },
+});
+console.log(token);
+console.log(user);
+/* if(email){
+  user = await client.user.findUnique({
+    where:{
+      email,
+    },
+  });
+  if(user )console.log("found it");
+  if(!user){
+    console.log("did not find. will create.");
+    user = await client.user.create({
+      data:{
+        name:"anonymous",
+        email,
+      }
+    });
+  }
+  console.log(user);
+}
+if(phone){
+  user = await client.user.findUnique({
+    where:{
+      phone:+phone,
+    },
+  });
+  if(user )console.log("found it");
+  if(!user){
+    console.log("did not find. will create.");
+    user = await client.user.create({
+      data:{
+        name:"anonymous",
+        phone:+phone,
+      }
+    });
+  }
+  console.log(user);
+} */
+return res.status(200).end();
 }
 
 export default withHandler("POST", handler);
